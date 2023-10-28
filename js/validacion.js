ca6f5b8575caf19c7d6861a24266d623f10289f6
guzb04 <guzmanbeyhaut@gmail.com>
2023-10-28 10:54:29 -0300

    minimo password = 6

--- js/validacion.js
+++ js/validacion.js
@@ -6,8 +6,9 @@ const registroButton = document.getElementById('registroButton');
 
 
 registroButton.addEventListener('click', () => {
-    if(password1.value == password2.value && password1.value.length ) {
+    if(password1.value == password2.value && password1.value.length>=6 ) {
         alert("funcionen!");
+        console.log("dsadas")
     }
 })
 
