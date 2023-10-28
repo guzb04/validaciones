2b4309709a984a9ee6f661f7fc0d1ee1fcdb1869
ivsoca <socanter23@gmail.com>
2023-10-28 10:49:01 -0300

    validacion contraseña en camino

---
+++ js/validacion.js
@@ -0,0 +1,13 @@
+const password1 = document.getElementById('password1');
+
+const password2 = document.getElementById('password2');
+
+const registroButton = document.getElementById('registroButton');
+
+
+registroButton.addEventListener('click', () => {
+    if(password1.value == password2.value && password1.value.length ) {
+        alert("funcionen!");
+    }
+})
+
