d084b20eabb4abaa0569ad9aa433818edd247993
ivsoca <socanter23@gmail.com>
2023-10-28 11:18:52 -0300

    probando validacion mail

--- js/validacion.js
+++ js/validacion.js
@@ -1,12 +1,18 @@
 const password1 = document.getElementById('password1');
 const password2 = document.getElementById('password2');
+const email = document.getElementById('email');
+
 const registroButton = document.getElementById('registroButton');
+
 
 
 registroButton.addEventListener('click', () => {
     if(password1.value == password2.value && password1.value.length>=6 ) {
         alert("funcionen!");
-        console.log("dsadas")
+    }
+
+    if(email.value ==1){
+        alert("dsdsdsds")
     }
 })
 
