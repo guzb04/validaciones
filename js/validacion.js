a34eb0acceba8cf6b927b4212bddc1d209436c17
guzb04 <guzmanbeyhaut@gmail.com>
2023-10-28 11:17:42 -0300

    inputs no pueden estar vacios

--- js/validacion.js
+++ js/validacion.js
@@ -1,14 +1,38 @@
-const password1 = document.getElementById('password1');
+document.addEventListener("DOMContentLoaded", ()=>{
+    const nombre = document.getElementById("nombre");
+    const apellido = document.getElementById("apellido");
+    const email = document.getElementById("email");
+    const password1 = document.getElementById('password1');
+    const password2 = document.getElementById('password2');
+    const registroButton = document.getElementById('registroButton');
+    const inputArray = [
+        nombre,
+        apellido,
+        email,
+        password1,
+        password2
+    ]
+    var hasClicked = 0
 
-const password2 = document.getElementById('password2');
 
-const registroButton = document.getElementById('registroButton');
+    registroButton.addEventListener('click', () => {
+        hasClicked = 1
+        checkValidity(inputArray)
+        console.log(inputArray)
+        console.log(hasClicked)
 
-
-registroButton.addEventListener('click', () => {
-    if(password1.value == password2.value && password1.value.length>=6 ) {
-        alert("funcionen!");
-        console.log("dsadas")
-    }
+    })
 })
 
+function checkValidity(inputArray){
+    if(password1.value == password2.value && password1.value.length>=6 ) {
+        alert("funcionen!");
+    }
+
+    inputArray.forEach(input => {
+        console.log(input.value)
+        if(!input.value){
+            input.classList.add("is-invalid")
+        }
+    });
+}
